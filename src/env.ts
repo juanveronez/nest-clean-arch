36c856f8e00de9545b4f720de493e341f26f77f3
Juan Veronez <juan.monteirov@gmail.com>
2026-05-06 16:17:47 -0300

    feat: create auth module using registerAsync to load config service before creating the module

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ config({ path: '.env.development' })
 export const envSchema = z.object({
   DATABASE_URL: z.url().startsWith('postgres://'),
   PORT: z.coerce.number().optional().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>

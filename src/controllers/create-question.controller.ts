436b73402dcd6c70af9f384f80f5a977eedf8898
Juan Veronez <juan.monteirov@gmail.com>
2026-05-08 19:05:42 -0300

    feat: create a custom param decorator to get current user and use it into create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,23 +1,14 @@
-import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-import z from 'zod'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from '../auth/current-user.decorator'
+import type { UserPayload } from '../auth/jwt.strategy'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
-import { ZodValidationPipe } from '../pipes/zod-validation.pipe'
-import { PrismaService } from '../prisma/prisma.service'
-
-const createQuestionSchema = z.object({})
-
-type CreateQuestionSchema = z.infer<typeof createQuestionSchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
-
   @Post()
-  @UsePipes(new ZodValidationPipe(createQuestionSchema))
-  handle(@Body() body: CreateQuestionSchema) {
-    const { content, slug, title, authorId } = body
+  handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
 
     return { ok: true }
   }

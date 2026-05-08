6e8fda23e6694378f0d3749716d3adb6380d258c
Juan Veronez <juan.monteirov@gmail.com>
2026-05-08 19:19:23 -0300

    feat: update create question controller using a pipe into body decorator and create a valid question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import z from 'zod'
 import { CurrentUser } from '../auth/current-user.decorator'
 import type { UserPayload } from '../auth/jwt.strategy'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { ZodValidationPipe } from '../pipes/zod-validation.pipe'
+import { PrismaService } from '../prisma/prisma.service'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  @Post()
-  handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
+  constructor(private prisma: PrismaService) {}
 
-    return { ok: true }
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    // Body can accept pipes to validate content directly
+    @Body(bodyValidationPipe) body: CreateQuestionBody,
+  ) {
+    const { title, content } = body
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        authorId: user.sub,
+        slug: title,
+      },
+    })
   }
 }

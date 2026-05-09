2eb5148e9308eb77aa6c16a8d9d9400c0430bcc6
Juan Veronez <juan.monteirov@gmail.com>
2026-05-09 15:05:11 -0300

    feat: update vitest config for e2e and unit tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,5 +1,4 @@
 import swc from 'unplugin-swc'
-import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -10,11 +9,14 @@ export default defineConfig({
     setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
-    tsConfigPaths(),
     // This is required to build the test files with SWC
     swc.vite({
       // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
       module: { type: 'es6' },
     }),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  oxc: false,
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,4 @@
 import swc from 'unplugin-swc'
-import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -8,11 +7,14 @@ export default defineConfig({
     root: './',
   },
   plugins: [
-    tsConfigPaths(),
     // This is required to build the test files with SWC
     swc.vite({
       // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
       module: { type: 'es6' },
     }),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  oxc: false,
 })

26c5a3cb64fbd63d3d0385f350f452e95bb8979f
Juan Veronez <juan.monteirov@gmail.com>
2026-05-22 21:28:56 -0300

    feat: add presenter layer for questions

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import z from 'zod'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
 import { ZodValidationPipe } from '../pipes/zod-validation.pipe'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -24,8 +25,16 @@ export class FetchRecentQuestions {
   async handle(
     @Query('page', pageQueryParamValidationPipe) page: PageQueryParam,
   ) {
-    const questions = await this.fetchRecentQuestions.execute({ page })
+    const result = await this.fetchRecentQuestions.execute({ page })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+
+    const { questions } = result.value
+
+    return {
+      questions: questions.map(QuestionPresenter.toHTTP),
+    }
   }
 }

66d2c058e1507a2c4a2d1b5e5c0d9d62aa3bc8d9
Juan Veronez <juan.monteirov@gmail.com>
2026-08-24 09:04:28 -0300

    feat: creates redis cache repository and export it in cache module

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
 import { EnvService } from '../env/env.service'
+import { CacheRepository } from './cache-repository'
 import { RedisService } from './redis/redis.service'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
 
 @Module({
-  providers: [EnvService, RedisService],
+  providers: [
+    EnvService,
+    RedisService,
+    { provide: CacheRepository, useClass: RedisCacheRepository },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
---
+++ src/infra/cache/redis/redis-cache-repository.ts
@@ -0,0 +1,20 @@
+import { CacheRepository } from '../cache-repository'
+import { RedisService } from './redis.service'
+
+const EXPIRATION_IN_SECONDS = 60 * 60 // 1 hour
+
+export class RedisCacheRepository implements CacheRepository {
+  constructor(private redis: RedisService) {}
+
+  async set(key: string, value: string): Promise<void> {
+    await this.redis.set(key, value, 'EX', EXPIRATION_IN_SECONDS)
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.redis.get(key)
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key)
+  }
+}
